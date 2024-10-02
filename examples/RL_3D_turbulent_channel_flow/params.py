ef96227d3bf4d43ea4ed0f03f0eeb6d4ea6c72bb
nmasclans <nuria.masclans@upc.edu>
2024-10-02 10:28:31 +0200

    change training param t_action

--- examples/RL_3D_turbulent_channel_flow/params.py
+++ examples/RL_3D_turbulent_channel_flow/params.py
@@ -3,7 +3,7 @@ import random, os, numpy as np
 # TODO: set custom values
 # t_phys  = delta / u_tau = 1
 dt_phys  = 5.0e-5       # not taken from here, defined in myRHEA.cpp
-t_action = 0.00100      # action period
+t_action = 0.00050      # action period
 t_begin_control = 0.0   # controls begin after this value
 t_episode_train = round(0.05 + t_action + dt_phys, 8)
 t_episode_eval = 1.0
