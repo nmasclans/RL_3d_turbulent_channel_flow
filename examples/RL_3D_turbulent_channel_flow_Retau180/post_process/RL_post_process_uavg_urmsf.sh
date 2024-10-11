d9127fd6314595289221fc40537312f475bdd67d
nmasclans <nuria.masclans@upc.edu>
2024-10-11 21:04:24 +0200

    change printing param

--- examples/RL_3D_turbulent_channel_flow_Retau180/post_process/RL_post_process_uavg_urmsf.sh
+++ examples/RL_3D_turbulent_channel_flow_Retau180/post_process/RL_post_process_uavg_urmsf.sh
@@ -12,7 +12,7 @@ source activate python3_9
 # Loop iteration number from '2820500' to '2822000' in steps of 500
 training_name=$1    # file input
 ensemble=0          # fixed
-for iter in {2820500..2822000..500}
+for iter in {2821000..2824000..1000}
 do
     # Run the Python post-processing script with the current iteration and the training name
     python3 RL_post_process_uavg_urmsf.py $iter $ensemble $training_name
