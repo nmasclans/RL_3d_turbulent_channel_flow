2390fe695de185c0aad2d923bcb9909883da752f
nmasclans <nuria.masclans@upc.edu>
2024-10-16 14:52:44 +0200

    params for 3 cases

--- examples/RL_3D_turbulent_channel_flow_Retau100_II/params.py
+++ examples/RL_3D_turbulent_channel_flow_Retau100_II/params.py
@@ -2,7 +2,7 @@ import random, os, numpy as np
 
 # t_phys  = delta / u_tau = 1
 dt_phys  = 1.0e-4       # not taken from here, defined in myRHEA.cpp
-t_action = 0.1          # action period
+t_action = 0.01         # action period
 t_begin_control = 0.0   # controls begin after this value
 t_episode_train = round(1.0 + t_action + dt_phys, 8)
 t_episode_eval = 1.0
@@ -12,7 +12,7 @@ mode = "train"          # "train" or "eval"
 
 params = {
     # smartsim params
-    "run_id": "",50
+    "run_id": "",
     "rhea_exe": "RHEA.exe",
     "rhea_case_path": os.environ["RHEA_CASE_PATH"],
     "rl_case_path": os.environ["RL_CASE_PATH"],
