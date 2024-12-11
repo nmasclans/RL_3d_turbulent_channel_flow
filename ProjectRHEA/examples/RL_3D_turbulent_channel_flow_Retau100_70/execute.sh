07ff3675faa6dbdeb7262b05da9fa931393091c8
nmasclans <nuria.masclans@upc.edu>
2024-12-11 11:48:25 +0100

    delete several cases

--- ProjectRHEA/examples/RL_3D_turbulent_channel_flow_Retau100_70/execute.sh
+++
@@ -1,2 +0,0 @@
-#mpirun -np 8 --hostfile my-hostfile ./RHEA.exe configuration_file.yaml
-mpirun --mca btl_base_warn_component_unused 0 -np 8 --hostfile my-hostfile ./RHEA.exe configuration_file.yaml
