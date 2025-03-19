68581c03578a54c85e1e64c2eb72d2c0ed228535
nmasclans <nuria.masclans@upc.edu>
2025-03-19 13:08:56 +0100

    save availbale restart files

---
+++ ProjectRHEA/available_restart_files/post_processing_bulk_values.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import sys
+import os
+import numpy as np
+import h5py    
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Bbox
+from matplotlib import rc,rcParams
+import matplotlib.colors as colors
+from matplotlib import ticker
+import matplotlib.cm as cm
+
+
+### Open data file
+for iter in [3210000, 3320000, 3520000, 3620000, 3720000, 3820000, 3920000, 4020000, 4120000]:
+    filename  = f'restart_data_file_{iter}.h5'
+    print(f"\nFilename: {filename}")
+    with h5py.File(filename, 'r') as data_file:
+        #list( data_file.keys() )
+        x_data        = data_file['x'][:,:,:]
+        y_data        = data_file['y'][:,:,:]
+        z_data        = data_file['z'][:,:,:]
+        u_data        = data_file['u'][:,:,:]
+        num_points_x  = u_data[0,0,:].size
+        num_points_y  = u_data[0,:,0].size
+        num_points_z  = u_data[:,0,0].size
+
+    ### Reference parameters
+    rho_0  = 1.0				# Reference density [kg/m3]
+    u_tau  = 1.0				# Friction velocity [m/s]
+    delta  = 1.0				# Channel half-height [m]
+    Re_tau = 100.0				# Friction Reynolds number [-]
+    mu_ref = rho_0*u_tau*delta/Re_tau	# Dynamic viscosity [Pa s]
+    nu_ref = mu_ref/rho_0			# Kinematic viscosity [m2/s]
+    L_x = 4.0*np.pi*delta
+
+
+    ### Average variables in space
+    sum_u_volume = 0.0
+    sum_volume = 0.0
+    for i in range( 1, num_points_x-1 ):
+        for j in range( 1, num_points_y-1 ):
+            for k in range( 1, num_points_z-1 ):
+                delta_x = 0.5*( x_data[k,j,i+1] - x_data[k,j,i-1] )
+                delta_y = 0.5*( y_data[k,j+1,i] - y_data[k,j-1,i] )
+                delta_z = 0.5*( z_data[k+1,j,i] - z_data[k-1,j,i] )
+                volume = delta_x*delta_y*delta_z
+                sum_u_volume += volume*u_data[k,j,i]
+                sum_volume += volume
+    U_b = sum_u_volume/sum_volume
+    print( "U_bulk: ", U_b )
+    #print( "L_x / U_bulk: ", L_x/U_b )
