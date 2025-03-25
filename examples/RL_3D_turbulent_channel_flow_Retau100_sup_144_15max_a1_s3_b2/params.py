165dc08e160203f82cdc24bd327a02b5324a9b86
nmasclans <nuria.masclans@upc.edu>
2025-03-25 11:10:31 +0100

    normalize action bounds to -1,1 in the rl framework

--- examples/RL_3D_turbulent_channel_flow_Retau100_sup_144_15max_a1_s3_b2/params.py
+++ examples/RL_3D_turbulent_channel_flow_Retau100_sup_144_15max_a1_s3_b2/params.py
@@ -56,7 +56,7 @@ params = {
     "t_action": t_action,
     "t_episode": t_episode_train if run_mode == "train" else t_episode_eval,
     "t_begin_control": t_begin_control,
-    "action_bounds": (-20.0, 20.0),
+    "action_bounds": (-1.0, 1.0),
     "action_dim": 1,
     "state_dim": 3,
     "reward_norm": 1.0,
