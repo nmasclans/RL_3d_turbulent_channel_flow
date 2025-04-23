fa819e56f56d501e72a7efc281ef4c8968976a83
nmasclans <nuria.masclans@upc.edu>
2025-04-23 12:49:08 +0200

    mods execute file

--- examples/RL_3D_execute/executeU.sh
+++ examples/RL_3D_execute/executeU.sh
@@ -2,7 +2,12 @@
 
 REPO_EXAMPLE_DIR=/home/jofre/Nuria/repositories/RL_3d_turbulent_channel_flow/examples
 
-for i in {16..20}; do
+for i in {10..20}; do
+
+    if [ "$i" -eq 16 ]; then
+        continue
+    fi
+
     echo "Running Case $i..."
 
     CASE_DIR_AUX="$REPO_EXAMPLE_DIR/RL_3D_turbulent_channel_flow_Retau100_sup_144_15max_TCP3_a3_s5_U$i"
@@ -11,7 +16,7 @@ for i in {16..20}; do
 
     bash ./run_train_triton.sh > "train_case_$i.out" 2>&1
 
-    echo "Finished Case $i."
+    echo "Finished Case U$i."
 done
 
-echo "Finish running execute.sh!"
+echo "Finish running executeU.sh!"
