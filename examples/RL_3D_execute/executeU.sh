93823d6a16c5f1832d08dfc9edc59091f509f461
nmasclans <nuria.masclans@upc.edu>
2025-05-09 23:08:58 +0200

    new unsupervised cases

--- examples/RL_3D_execute/executeU.sh
+++ examples/RL_3D_execute/executeU.sh
@@ -3,11 +3,11 @@
 REPO_EXAMPLE_DIR=/home/jofre/Nuria/repositories/RL_3d_turbulent_channel_flow/examples
 
 for i in {10..12}; do
-    for j in {2..5}; do
+    for j in {1..5}; do
 
         echo "Running Case U${i}_${j}..."
 
-        CASE_DIR_AUX="$REPO_EXAMPLE_DIR/RL_3D_turbulent_channel_flow_Retau100_sup_144_15max_TCP3_a3_s5_U${i}_${j}"
+        CASE_DIR_AUX="$REPO_EXAMPLE_DIR/RL_3D_turbulent_channel_flow_Retau100_U${i}_${j}"
 
         cd "$CASE_DIR_AUX" || { echo "Failed to enter $CASE_DIR_AUX"; exit 1; }
 
