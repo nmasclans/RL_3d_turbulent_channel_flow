e46eebfc3496afb48601dd1f308017a66fa4563f
nmasclans <nuria.masclans@upc.edu>
2025-09-17 11:31:37 +0200

    larger evaluation time

--- examples/RL_3D_turbulent_channel_flow_Retau180_eval_S/params.py
+++ examples/RL_3D_turbulent_channel_flow_Retau180_eval_S/params.py
@@ -4,8 +4,8 @@ import time, random, os, numpy as np
 dt_phys  = 0.5e-5       # not taken from here, defined in myRHEA.cpp
 t_action = 0.0278       # action period
 t_begin_control = 0.0   # controls begin after this value
-t_episode_train = round(5.0 + t_action + dt_phys, 8)
-t_episode_eval = round(5.0 + t_action + dt_phys, 8)
+t_episode_train = round(10.0 + t_action + dt_phys, 8)
+t_episode_eval = round(10.0 + t_action + dt_phys, 8)
 cfd_n_envs = 1          # num. cfd simulations run in parallel
 rl_n_envs = 160         # num. regions del domini en wall-normal direction -> gets the witness points
 run_mode = os.environ["RUN_MODE"]          # "train" or "eval"
